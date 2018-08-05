d37c5a42b82d6b54f79e46d1da416a7ec31a329d
Jan Erik Herrmann <jan@nw-sys.de>
2018-08-05 21:29:19 +0200

    add test annotation functionality

--- src/app/annotator/annotator.component.ts
+++ src/app/annotator/annotator.component.ts
@@ -7,9 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnnotatorComponent implements OnInit {
 
+  selectedText = '';
+  documentText = Array('Hier', 'steht', 'ganz',  'viel', 'Text', 'den', 'man', 'selektieren', 'kann', '.');
+  annotations = [];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  calcClass(index) {
+    if (this.annotations[index]) {
+      return 'redEntity';
+    } else {
+      return 'default';
+    }
+  }
+
+  toggleTag(index) {
+    if (this.annotations[index]) {
+      this.selectedText = '';
+      this.annotations[index] = 0;
+    } else {
+      this.selectedText = this.documentText[index];
+      this.annotations[index] = 1;
+    }
   }
 
 }
