45794eb6dd4ce715d563fc3ac1c164ef4b7b3a77
Jan Erik Herrmann <jan@nw-sys.de>
2018-08-05 21:40:47 +0200

    add material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppComponent } from './app.component';
 import { AnnotatorComponent } from './annotator/annotator.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AnnotatorComponent } from './annotator/annotator.component';
     AnnotatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
