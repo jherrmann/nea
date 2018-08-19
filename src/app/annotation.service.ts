eed1061955b9a09e160e9493ae56d066432086b5
Jan Erik Herrmann <jan@nw-sys.de>
2018-08-19 16:57:53 +0200

    add simple regex tokenizer

--- src/app/annotation.service.ts
+++ src/app/annotation.service.ts
@@ -10,7 +10,12 @@ export class AnnotationService {
 
   getAnnotation(id: number) {
     const anno = new Annotation();
-    anno.tokens = Array('Hier', 'steht', 'ganz',  'viel', 'Text', 'den', 'man', 'selektieren', 'kann', '.');
+    // Array('Hier', 'steht', 'ganz',  'viel', 'Text', 'den', 'man', 'selektieren', 'kann', '.');
+    anno.text = 'XYZ grants an early payment discount of two percent (2%) when paid until 10 days after receive of the invoice.';
+    anno.tokens = this.tokenize(anno.text);
     return anno;
+  }
+  private tokenize(text: string) {
+    return text.split(/(\s|\(|\)|\[|\]|%)/g);
   }
 }
