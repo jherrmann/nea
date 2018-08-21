d8955bf3ac5b4e11486daabcd1f087e710713c4c
Jan Erik Herrmann <jan@nw-sys.de>
2018-08-21 21:07:10 +0200

    Start Highlight correct tokens

--- client/src/app/annotation.service.ts
+++ client/src/app/annotation.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 import { Observable, of } from 'rxjs';
-import { Annotation } from './annotation';
+import { Annotation, NamedEntity } from './annotation';
+import { EntityType } from './entitytype';
 
 @Injectable({
   providedIn: 'root'
@@ -21,13 +22,46 @@ export class AnnotationService {
         anno.text = result['text'];
         anno.tokens = this.tokenize(result['text']);
         // TODO fill correctly
-        anno.entities = [];
+        anno.entities = this.setEntitiesForTokens(anno.tokens, result['named_entities']);
         return anno;
       }),
       catchError(err => { throw Error(err); })
     );
   }
-  private tokenize(text: string) {
+  private tokenize(text: string): Array<string> {
     return text.split(/(\s|\(|\)|\[|\]|%)/g);
+  }
+
+  private setEntitiesForTokens(tokens: Array<string>, namedEntities: Array<NamedEntity>) {
+    const entities = [];
+    if (namedEntities) {
+      const nrOfEntites = namedEntities.length;
+      let currentEntityIdx = 0;
+      // sort so we dont have to scan the named entities
+      namedEntities.sort((a, b) => b.value.localeCompare(a.value));
+      if (nrOfEntites > 0) {
+        let currentStart = 0;
+        let currentEnd = 0;
+        let currentEntity =  namedEntities[currentEntityIdx];
+
+        for (const element of tokens) {
+          currentStart = currentEnd;
+          currentEnd += element.length;
+
+          if (currentEntity.begin >= currentStart && currentEntity.end <= currentEnd) {
+            // TODO get entitytype from service
+            entities.push(new EntityType(currentEntity.entity, 'red'));
+            currentEntityIdx++;
+            currentEntity = namedEntities[currentEntityIdx];
+          } else {
+            entities.push(null);
+          }
+          if (currentEntityIdx === nrOfEntites) {
+            break;
+          }
+        }
+      }
+    }
+    return entities;
   }
 }
